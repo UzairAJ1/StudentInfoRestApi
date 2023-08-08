0f55101bbf2fd4cdc35e043f51f4d272d32c3bc1
Uzair Aj <53966756+UzairAJ1@users.noreply.github.com>
2023-08-08 23:40:53 +0500

    Update firstController.java

--- src/main/java/com/example/demo/firstController.java
+++ src/main/java/com/example/demo/firstController.java
@@ -1,14 +1,1 @@
-package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class firstController {
-	@GetMapping("/hello")
-	public String hello()
-	{
-		return "hello Uzair";
-	}
-
-}
